4a9c2a389b63a837cb94c6e1779065aa38528d6b
Juice Milner <116606682+justinjmilner@users.noreply.github.com>
2022-11-28 14:12:53 -0800

    Add files via upload

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    alchemyAPI: "",
+    lotteryTokenAddress: "0xB46C4e94fbB1e447a0E64F37CDE249F65E5147D3",
+    lotteryAddress: "0x8f1c4b0bb325ba77029aa172cb7c48746ee8faed",
+  };
